e75667a325e5bfb2c44066b17ff0d38c14d0c63f
RafalM2000 <rfm@europe.com>
2021-01-09 12:41:40 +0100

    counter in app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'changeDetection';
+  paretnCounter = 0;
+
+  ngOnInit(): void {
+    setInterval(() => {this.paretnCounter++}, 500)
+  }
 }
