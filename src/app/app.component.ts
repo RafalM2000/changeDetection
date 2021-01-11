6fdfe4b6c7c56cbc952950164c26a41fd14c7c84
RafalM2000 <rfm@europe.com>
2021-01-11 20:28:46 +0100

    zmiany wymagane przez TsLint

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   paretnCounter = 0;
 
   ngOnInit(): void {
-    setInterval(() => {this.paretnCounter++}, 500)
+    setInterval(() => {this.paretnCounter++; }, 500);
   }
 }
